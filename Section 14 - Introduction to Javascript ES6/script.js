12d0401542b62f4781488c5b5be4e785a9c515ca
Manoj Gawade <Gawade.manoj7@gmail.com>
2025-12-30 22:39:09 +0530

    77. Data Types

--- Section 14 - Introduction to Javascript ES6/script.js
+++ Section 14 - Introduction to Javascript ES6/script.js
@@ -1,1 +1,8 @@
-alert("Hello, welcome to JavaScript ES6!");
+// 76. JavaScript alert
+
+// alert("Hello, welcome to JavaScript ES6!");
+
+// 77. Data Types
+
+var myString = "Hello, World!"; // String
+concole.log(typeof myString); // Output: string
