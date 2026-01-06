77d18a318e60e51b2872c95a724570f1ddc9c6fc
Manoj Gawade <Gawade.manoj7@gmail.com>
2026-01-06 06:48:11 +0530

    Dicee Challenge - Starting Files Added

--- Section 14 - Introduction to Javascript ES6/script.js
+++ Section 14 - Introduction to Javascript ES6/script.js
@@ -6,3 +6,17 @@
 
 var myString = "Hello, World!"; // String
 concole.log(typeof myString); // Output: string
+
+// 78. JavaScript Variables
+
+var myNumber = 42; // Number
+console.log(typeof myNumber); // Output: number
+var myBoolean = true; // Boolean
+console.log(typeof myBoolean); // Output: boolean
+var myUndefined; // Undefined
+console.log(typeof myUndefined); // Output: undefined
+var myNull = null; // Null
+console.log(typeof myNull); // Output: object
+
+
+
