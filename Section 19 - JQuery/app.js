94a4ae3651b96fdd15aa6bfa3bd1afd0df593d38
Manoj Gawade <Gawade.manoj7@gmail.com>
2026-01-27 23:41:01 +0530

    156. Adding Event Listeners with jQuery

--- Section 19 - JQuery/app.js
+++ Section 19 - JQuery/app.js
@@ -11,3 +11,22 @@ $("button").html("<em>Click Me</em>");
 
 // 155. Manipulating Attributes with jQuery
 $("a").attr("href", "https://www.yahoo.com");
+
+
+// 156. Adding Event Listeners with jQuery
+$("h1").click(function () {
+    $("h1").css("color", "purple");
+});
+
+$("button").click(function () {
+    $("h1").css("color", "orange");
+});
+
+$(document).keypress(function (event) {
+    $("h1").text(event.key);
+});
+
+$("h1").on("mouseover", function () {
+    $("h1").css("color", "pink");
+});
+
