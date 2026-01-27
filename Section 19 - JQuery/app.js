70a56861f56ec6f55f9691e3b575f698bd349723
Manoj Gawade <Gawade.manoj7@gmail.com>
2026-01-27 23:26:31 +0530

    154. Manipulating Text with jQuery

--- Section 19 - JQuery/app.js
+++ Section 19 - JQuery/app.js
@@ -1,4 +1,10 @@
+// 153. Manipulating Styles with jQuery
 // alert("Hello JQuery");
 // $("h1").css("color", "red");
 $("h1").addClass("big-title margin-50");
 $("p").css("color", "blue");
+
+// 154. Manipulating Text with jQuery
+$("h1").text("Bye");
+$("button").html("<em>Click Me</em>");
+$("button").html("<em>Click Me</em>");
