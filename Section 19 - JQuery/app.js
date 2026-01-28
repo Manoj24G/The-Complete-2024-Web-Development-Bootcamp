80fae220a37355a6ea53224f3a67c01198482773
Manoj Gawade <Gawade.manoj7@gmail.com>
2026-01-28 06:18:33 +0530

    162. Simon Game staring files added.

--- Section 19 - JQuery/app.js
+++ Section 19 - JQuery/app.js
@@ -33,7 +33,29 @@ $("h1").on("mouseover", function () {
 //157. Adding and Removing Elements with jQuery
 $("h1").before("<button>Before</button>");
 $("h1").after("<button>After</button>");
-$("h1").prepend("<button>Prepend</button>");
-$("h1").append("<button>Append</button>");
+// $("h1").prepend("<button>Prepend</button>");
+// $("h1").append("<button>Append</button>");
 
 // $("button").remove();
+
+
+// 158. Animations with jQuery
+
+$("button").on("click", function () {
+    // $("h1").hide();
+    // $("h1").show();
+    // $("h1").toggle();
+    // $("h1").fadeOut();
+    // $("h1").fadeIn();
+    // $("h1").fadeToggle();
+    // $("h1").slideUp();
+    // $("h1").slideDown();
+    // $("h1").slideToggle();
+    // $("h1").animate({ opacity: 0.5 });
+    // $("h1").animate({ margin: "20px" });
+    $("h1").slideUp().slideDown().animate({ opacity: 0.5 });
+});
+
+// Note: You can chain multiple animate methods but only one property can be animated at a time using animate() method.
+
+
