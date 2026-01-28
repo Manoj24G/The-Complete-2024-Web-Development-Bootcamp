2f2fa12f6cd153fcb0a8a6f8b2226f6e6ee7d173
Manoj Gawade <Gawade.manoj7@gmail.com>
2026-01-28 05:52:38 +0530

    157. Adding and Removing Elements with jQuery

--- Section 19 - JQuery/app.js
+++ Section 19 - JQuery/app.js
@@ -30,3 +30,10 @@ $("h1").on("mouseover", function () {
     $("h1").css("color", "pink");
 });
 
+//157. Adding and Removing Elements with jQuery
+$("h1").before("<button>Before</button>");
+$("h1").after("<button>After</button>");
+$("h1").prepend("<button>Prepend</button>");
+$("h1").append("<button>Append</button>");
+
+// $("button").remove();
